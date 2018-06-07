0593fb3c79fb8d8f0857785d01b048af1a3f0722
banderas <banderas328@yandex.ru>
2018-06-07 15:25:37 +0300

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react';
+
+function Title(props){
+    return  <h1>{props.message}</h1>
+}
+
+// class Title extends Component {
+//     render() {
+//         return  <h1>{this.props.message}</h1>
+//     }
+// }
+
+export default Title
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import ReactDOM from 'react-dom';
+import Main from "./components/Main"
+import './components/styles/styles.css'
+import {BrowserRouter} from 'react-router-dom'
+
+
+
+
+
+
+
+
+ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'))
+    
+
+
